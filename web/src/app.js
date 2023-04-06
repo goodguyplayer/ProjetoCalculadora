d28fe7472848e66c830b0e81602e0d5cf7a1de0e
goodguyplayer <nathanbritodasilva@gmail.com>
2023-04-06 10:47:30 -0300

    Started basics of web section

---
+++ web/src/app.js
@@ -0,0 +1,7 @@
+const express = require('express');
+const jquery = require('jquery');
+const app = express();
+const router = express.Router();
+
+const path = __dirname + '/views/';
+const port = 5000;
