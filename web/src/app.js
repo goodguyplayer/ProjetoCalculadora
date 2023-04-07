7e701253bfe4974ab07a6c2b2d0515521ce8bc1c
goodguyplayer <nathanbritodasilva@gmail.com>
2023-04-06 21:06:48 -0300

    Web - moved files, managed to get docker to run

--- web/src/app.js
+++
@@ -1,7 +0,0 @@
-const express = require('express');
-const jquery = require('jquery');
-const app = express();
-const router = express.Router();
-
-const path = __dirname + '/views/';
-const port = 5000;
