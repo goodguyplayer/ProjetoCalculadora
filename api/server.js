0cc92d03f1f263302711fdfa1df6df41f677bcba
goodguyplayer <nathanbritodasilva@gmail.com>
2023-04-08 20:49:06 -0300

    Server.js updated

--- api/server.js
+++ api/server.js
@@ -1,5 +1,6 @@
 const fs = require("fs/promises");
 const express = require("express");
+const bodyParser = require('body-parser');
 const cors = require("cors");
 const _ = require("lodash");
 const {v4: uuid} = require("uuid");
@@ -7,5 +8,17 @@ const {v4: uuid} = require("uuid");
 const PORT = 5050;
 
 const app = express();
+
+app.use(bodyParser.json());
+
+app.use(cors());
+
+app.get('/', (req, res) => {
+    res.send("");
+  });
+
+app.get("/calculate", (req, res) =>{
+    console.log("This works")
+});
 
 app.listen(PORT, () => console.log("API server, start"));
