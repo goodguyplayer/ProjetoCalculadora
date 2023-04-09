d5f3f7957a2083a35ecbe422c1d6ebc28ba10365
goodguyplayer <nathanbritodasilva@gmail.com>
2023-04-09 15:18:21 -0300

    Changed calculator.js to use jquery

--- web/event-emitters/calculator.js
+++ web/event-emitters/calculator.js
@@ -6,20 +6,24 @@
 //var client = http.createClient(port, address);
 //var request = client.request('PUT', '/users/1');
 
-let result = document.getElementById("calculator_screen");
+//let result = document.getElementById("calculator_screen");
+let result = $("#calculator_screen");
+let username = $("#username");
+
+console.log(result);
 
 let calculate=(number)=>{
     if(typeof result == 'undefined'){
-        result.textContent = number;
+        result.text(number);
     }
     else{
-        result.textContent += number;
+        result.text(result.text() + number);        
     }
 }
 
 let erase=()=>{
     try{
-        result.textContent = result.textContent.slice(0, -1);
+        result.text(result.text().slice(0, -1));
     }
     catch(err){
 
@@ -27,19 +31,29 @@ let erase=()=>{
 }
 
 let eraseAll=()=>{
-    result.textContent = ""
+    result.text("");
 }
 
 let finishOperation=()=>{
     const toCalculate = {
-        username : document.getElementById("username").value,
-        operation : result.textContent,
+        "username" : username.text(),
+        "operation" : result.text(),
     };
-    fetch('/calculate', {method: 'POST'})
+    console.log(toCalculate);
+    fetch('/calculate', {
+        method: 'POST', 
+        headers: {
+            "Content-type": "application/json; charset=UTF-8"
+          },
+        body: JSON.stringify({
+            "username" : document.getElementById("username").value,
+            "operation" : result.textContent,
+        })
+    })
         .then(function(response) {
             if (response.ok){
                 console.log("Finish operation api called");
-                return toCalculate;
+                return;
             }
             throw new Error('Request failed');
         })
@@ -47,7 +61,7 @@ let finishOperation=()=>{
             console.log(error);
         });
 
-    console.log(toCalculate);
+    //console.log(toCalculate);
     try{
         result.textContent = eval(result.textContent);
     }
